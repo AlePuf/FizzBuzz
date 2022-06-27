96fbaab6b5ec6ccd553365cc89ffe9a9884e0540
Alex Puflene (Local Admin) <AlePufAdmin@softwire.com>
2022-06-27 14:08:17 +0100

    Added rules for division by 13 and 17 + support for user input

--- index.ts
+++ index.ts
@@ -1,23 +1,72 @@
-for (let index = 1; index <= 100; index++) {
-    let printed = 0;
-    if (index % 11 == 0) {
-        console.log("Bong");
-        continue;
+import * as readline from 'readline';
+
+let rl = readline.createInterface({
+   input: process.stdin,
+   output: process.stdout
+});
+
+rl.question('Insert number: ', (answer) => {
+    let n = Number(answer);
+    for (let index = 1; index <= n; index++) {
+        let printed = 0;
+        if (index % 17 == 0) {
+            if (index % 11 == 0) {
+                if (index % 13 == 0) {
+                    console.log("BongFezz");
+                } else {
+                    console.log("Bong");
+                }
+                continue;
+            }
+            if (index % 7 == 0) {
+                process.stdout.write("Bang");
+                printed = 1;
+            }
+            if (index % 5 == 0) {
+                process.stdout.write("Buzz");
+                printed = 1;
+            }
+            if (index % 13 == 0) {
+                process.stdout.write("Fezz");
+                printed = 1;
+            }
+            if (index % 3 == 0) {
+                process.stdout.write("Fizz");
+                printed = 1;
+            }
+            if (printed == 0) {
+                process.stdout.write(index.toString());
+            }
+        } else {
+            if (index % 11 == 0) {
+                if (index % 13 == 0) {
+                    console.log("FezzBong");
+                } else {
+                    console.log("Bong");
+                }
+                continue;
+            }
+            if (index % 3 == 0) {
+                process.stdout.write("Fizz");
+                printed = 1;
+            }
+            if (index % 13 == 0) {
+                process.stdout.write("Fezz");
+                printed = 1;
+            }
+            if (index % 5 == 0) {
+                process.stdout.write("Buzz");
+                printed = 1;
+            }
+            if (index % 7 == 0) {
+                process.stdout.write("Bang");
+                printed = 1;
+            }
+            if (printed == 0) {
+                process.stdout.write(index.toString());
+            }
+        }
+        console.log();
     }
-    if (index % 3 == 0) {
-        process.stdout.write("Fizz");
-        printed = 1;
-    }
-    if (index % 5 == 0) {
-        process.stdout.write("Buzz");
-        printed = 1;
-    }
-    if (index % 7 == 0) {
-        process.stdout.write("Bang");
-        printed = 1;
-    }
-    if (printed == 0) {
-        process.stdout.write(index.toString());
-    }
-    console.log();
-}
+    rl.close();
+});
