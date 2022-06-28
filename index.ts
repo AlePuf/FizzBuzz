59d70e766d0127edfbd74374aa86b3c5a8095fe7
Alex Puflene (Local Admin) <AlePufAdmin@softwire.com>
2022-06-28 08:34:58 +0100

    Functions + coding style

--- index.ts
+++ index.ts
@@ -5,66 +5,45 @@ let rl = readline.createInterface({
    output: process.stdout
 });
 
+function check(n: number, d: number, s: string) {
+    if (n % d == 0) {
+        process.stdout.write(s);
+        return 1;
+    }
+    return 0;
+}
+
+function check_11_13(n: number, s1: string, s2: string) {
+    if (n % 11 == 0) {
+        if (n % 13 == 0) {
+            console.log(s1);
+        } else {
+            console.log(s2);
+        }
+        return 1;
+    }
+    return 0;
+}
+
 rl.question('Insert number: ', (answer) => {
     let n = Number(answer);
     for (let index = 1; index <= n; index++) {
         let printed = 0;
         if (index % 17 == 0) {
-            if (index % 11 == 0) {
-                if (index % 13 == 0) {
-                    console.log("BongFezz");
-                } else {
-                    console.log("Bong");
-                }
-                continue;
-            }
-            if (index % 7 == 0) {
-                process.stdout.write("Bang");
-                printed = 1;
-            }
-            if (index % 5 == 0) {
-                process.stdout.write("Buzz");
-                printed = 1;
-            }
-            if (index % 13 == 0) {
-                process.stdout.write("Fezz");
-                printed = 1;
-            }
-            if (index % 3 == 0) {
-                process.stdout.write("Fizz");
-                printed = 1;
-            }
-            if (printed == 0) {
-                process.stdout.write(index.toString());
-            }
+            printed = check_11_13(index, "BongFezz", "Bong");
+            printed = check(index, 7, "Bang");
+            printed = check(index, 5, "Buzz");
+            printed = check(index, 13, "Fezz");
+            printed = check(index, 3, "Fizz");
         } else {
-            if (index % 11 == 0) {
-                if (index % 13 == 0) {
-                    console.log("FezzBong");
-                } else {
-                    console.log("Bong");
-                }
-                continue;
-            }
-            if (index % 3 == 0) {
-                process.stdout.write("Fizz");
-                printed = 1;
-            }
-            if (index % 13 == 0) {
-                process.stdout.write("Fezz");
-                printed = 1;
-            }
-            if (index % 5 == 0) {
-                process.stdout.write("Buzz");
-                printed = 1;
-            }
-            if (index % 7 == 0) {
-                process.stdout.write("Bang");
-                printed = 1;
-            }
-            if (printed == 0) {
-                process.stdout.write(index.toString());
-            }
+            printed = check_11_13(index, "FezzBong", "Bong");
+            printed = check(index, 3, "Fizz");
+            printed = check(index, 13, "Fezz");
+            printed = check(index, 5, "Buzz");
+            printed = check(index, 7, "Bang");
+        }
+        if (printed == 0) {
+            process.stdout.write(index.toString());
         }
         console.log();
     }
