04387efed0bceb1b0ae4533485364bd8a46cc16d
Alex Puflene <Alex.Puflene@softwire.com>
2022-06-28 10:05:53 +0100

    Added command line params

--- index.ts
+++ index.ts
@@ -12,17 +12,29 @@ function max(a: number, b: number) {
     return b;
 }
 
-function check(n: number, d: number, s: string) {
-    if (n % d == 0) {
+function checkIfArg(n: number, args: Array<number>) {
+    for (let i = 0; i < args.length; i++) {
+        if (n == args[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function check(n: number, d: number, s: string, args: Array<number>) {
+    let found = checkIfArg(d, args);
+    if (n % d == 0 && found == 1) {
         process.stdout.write(s);
         return 1;
     }
     return 0;
 }
 
-function check_11_13(n: number, s1: string, s2: string) {
-    if (n % 11 == 0) {
-        if (n % 13 == 0) {
+function check_11_13(n: number, s1: string, s2: string, args: Array<number>) {
+    let found_11 = checkIfArg(11, args);
+    if (n % 11 == 0 && found_11 == 1) {
+        let found_13 = checkIfArg(13, args);
+        if (n % 13 == 0 && found_13 == 1) {
             console.log(s1);
         } else {
             console.log(s2);
@@ -33,27 +45,32 @@ function check_11_13(n: number, s1: string, s2: string) {
 }
 
 rl.question('Insert number: ', (answer) => {
+    const myArgs = process.argv.slice(2);
+    let numArgs = [];
+    for (let i = 0; i < myArgs.length; i++) {
+        numArgs.push(parseInt(myArgs[i]));
+    }
     let n = Number(answer);
     for (let index = 1; index <= n; index++) {
         let printed = 0;
         if (index % 17 == 0) {
-            printed = max(printed, check_11_13(index, "BongFezz", "Bong"));
+            printed = max(printed, check_11_13(index, "BongFezz", "Bong", numArgs));
             if (printed == 1) {
                 continue;
             }
-            printed = max(printed, check(index, 7, "Bang"));
-            printed = max(printed, check(index, 5, "Buzz"));
-            printed = max(printed, check(index, 13, "Fezz"));
-            printed = max(printed, check(index, 3, "Fizz"));
+            printed = max(printed, check(index, 7, "Bang", numArgs));
+            printed = max(printed, check(index, 5, "Buzz", numArgs));
+            printed = max(printed, check(index, 13, "Fezz", numArgs));
+            printed = max(printed, check(index, 3, "Fizz", numArgs));
         } else {
-            printed = max(printed, check_11_13(index, "FezzBong", "Bong"));
+            printed = max(printed, check_11_13(index, "FezzBong", "Bong", numArgs));
             if (printed == 1) {
                 continue;
             }
-            printed = max(printed, check(index, 3, "Fizz"));
-            printed = max(printed, check(index, 13, "Fezz"));
-            printed = max(printed, check(index, 5, "Buzz"));
-            printed = max(printed, check(index, 7, "Bang"));
+            printed = max(printed, check(index, 3, "Fizz", numArgs));
+            printed = max(printed, check(index, 13, "Fezz", numArgs));
+            printed = max(printed, check(index, 5, "Buzz", numArgs));
+            printed = max(printed, check(index, 7, "Bang", numArgs));
         }
         if (printed == 0) {
             process.stdout.write(index.toString());
